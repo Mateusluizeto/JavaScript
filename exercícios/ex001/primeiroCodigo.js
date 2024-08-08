dd53bffe7504f4d2d1f092d4b25cef26e7fff7cb
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-08 18:05:15 -0300

    Meu primeiro código

---
+++ exercícios/ex001/primeiroCodigo.js
@@ -0,0 +1,11 @@
+ let mensagemTela = "Olá mundo! Meu nome é Mateus. Eu tenho 17 anos e meu sonho é ser programador";
+
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+console.log(mensagemTela)
+
+mensagemTela = "Agora eu quero ser padeiro";
+
+console.log(mensagemTela)

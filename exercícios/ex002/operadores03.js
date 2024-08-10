eecf312fec5dbcf58caa1bdf0c1717cb1d19e1e7
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-10 14:50:34 -0300

    Operadores booleanos

---
+++ exercícios/ex002/operadores03.js
@@ -0,0 +1,12 @@
+const valor1 = true;
+const valor2 = false;
+
+const valor1EValor2 = valor1 && valor2; /*Se os dois forem verdadeiros é true*/
+const valor1OUValor2 = valor1 || valor2; /*Se algum dos dois for verdadeiro é true*/
+const inversoValor1 = !valor1; /*Seu for true o resultado será false*/
+const inversoValor2 = !valor2; /*Seu for false o resultado será true*/
+
+console.log(valor1EValor2);
+console.log(valor1OUValor2);
+console.log(inversoValor1);
+console.log(inversoValor2)

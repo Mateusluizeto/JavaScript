af97523678ff4208a7947608153cbd7630f90bb9
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-14 18:39:35 -0300

    Objects em js

---
+++ exercícios/ex006/objeto.js
@@ -0,0 +1,14 @@
+const documentoIdentidade = {
+    nome: 'Daniel',
+    sobrenome: 'Porto',
+    cpf: '111.111.111-11',
+    empresa: 'Hashtag',
+    idade: 29
+}
+
+/*console.log(documentoIdentidade)*/
+
+console.log(documentoIdentidade.sobrenome)
+// Resultado: Porto
+console.log(documentoIdentidade['cpf'])
+// Resultado: 111.111.111-11

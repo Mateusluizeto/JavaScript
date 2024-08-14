1f66866e67aea2ec2af9202d918401515c71da50
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-14 19:43:50 -0300

    Exercicio com arrays e objetos

---
+++ exercícios/ex006/tipoReferencia.js
@@ -0,0 +1,17 @@
+let musica = {
+    nome: 'Let It Be',
+    artista: 'Beatles',
+    album: 'Let It Be',
+}
+
+let listasDeBandas = ['Beatles', 'Rolling Stones', 'Led Zeppelin', 'Cássia Eller', 'Chico Buarque',]
+
+console.log(listasDeBandas[4])
+listasDeBandas[5] = 'Skank'
+listasDeBandas[3] = 'Travis Scott'
+console.log(listasDeBandas[5])
+console.log(listasDeBandas[3])
+
+musica.album = 'Past Masters'
+musica.ano = 1979
+console.log(musica)

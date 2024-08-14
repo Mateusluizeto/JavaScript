ab39ca98976c88a098bb378a8477cacbacea86e5
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-14 19:05:40 -0300

    array em js

---
+++ exercícios/ex006/array.js
@@ -0,0 +1,18 @@
+const investimentoMensal = [1000, 2000, 3000, 2500, 1500, 1200, 1300,]
+const arrayDeStrings = ['String 1', 'String 2', 'String 3', 'String 4']
+
+console.log(`O valor investido no mês de janeiro foi ${investimentoMensal[0]}`)
+console.log(`O valor investido no mês de fevereiro foi ${investimentoMensal[1]}`)
+console.log(`O valor investido no mês de março foi ${investimentoMensal[2]}`)
+console.log(`O valor investido no mês de abril foi ${investimentoMensal[3]}`)
+console.log(`O valor investido no mês de maio foi ${investimentoMensal[4]}`)
+console.log(`O valor investido no mês de junho foi ${investimentoMensal[5]}`)
+console.log(`O valor investido no mês de julho foi ${investimentoMensal[6]}`)
+/* Resultado :  O valor investido no mês de janeiro foi 1000
+                O valor investido no mês de fevereiro foi 2000
+                O valor investido no mês de março foi 3000
+                O valor investido no mês de abril foi 2500
+                O valor investido no mês de maio foi 1500
+                O valor investido no mês de junho foi 1200
+                O valor investido no mês de julho foi 1300
+*/

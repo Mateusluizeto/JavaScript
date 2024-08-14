011516c0a63a34a2d787973c643360038d08455f
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-14 18:22:43 -0300

    undefined e null

---
+++ exercícios/ex005/tipos_valor.js
@@ -0,0 +1,12 @@
+/*console.log(typeof undefined)
+console.log(typeof null)*/
+
+let valor1 = null
+let valor2 = undefined
+console.log(valor1)
+console.log(typeof valor1)
+console.log(valor2)
+console.log(typeof valor2)
+// resultado: undefined
+//undefined
+

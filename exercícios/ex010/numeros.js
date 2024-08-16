74f74542f34d189e508ae5a13b6bdd0c2dfbb114
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-16 14:06:25 -0300

    funções

---
+++ exercícios/ex010/numeros.js
@@ -0,0 +1,8 @@
+let numeroDaVez = 1
+
+function calcularQuadrado() {
+    console.log(`O número da vez é o ${numeroDaVez}`)
+    console.log(`O número da vez (${numeroDaVez}) ao quadrado vale ${numeroDaVez ** 2}`)
+    numeroDaVez++
+}
+calcularQuadrado()

1c0777565c02f60db035840ea01286b4862a080b
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-08-20 19:32:50 -0300

    tipos de métodos

---
+++ exercícios/ex014/tiposDeMétodo.js
@@ -0,0 +1,10 @@
+let a = 2.00000001
+
+console.log(a)
+console.log(a.toFixed(4))
+console.log(a.toString())
+
+let b = 'Texto da aula de métodos'
+console.log(b.charAt(2))
+console.log(b.concat(' muito legal'))
+console.log(b.length)

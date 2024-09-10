61d3f859ad4c6eb27aab9959d9488214fc63247b
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-10 14:59:10 -0300

    Operadores lógicos: &&, || , !

---
+++ exercícios/ex009/OperadoresAritmeticos03.js
@@ -0,0 +1,15 @@
+// Operadores Lógicos (logical operators)
+
+// - 2 valores booleanos, quando verificados, resultara em verdadeiro ou falso
+
+let pao = true
+let queijo = false
+
+// AND &&
+console.log(pao && queijo)  // Os dois tem que ser positivo paraa dar true
+
+// OR ||
+console.log(pao || queijo) // Um dos dois tem que ser positivo para dar true
+
+// NOT !
+console.log(! queijo) // Se ele é verdadeiro vai se tronar falso e se ele for falso vai se tornar verdadeiro

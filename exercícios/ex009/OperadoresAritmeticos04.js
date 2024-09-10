d7b074410c2d7b6e872470f0e2c429c36fffab2b
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-10 14:59:47 -0300

    Operadores de atribuição ( +=, -=, *= )

---
+++ exercícios/ex009/OperadoresAritmeticos04.js
@@ -0,0 +1,26 @@
+// Operadores de atribuição (Assignment)
+let x
+
+
+// Assignment (vai atribuir o valor para x)
+x = 1
+
+// Addition assignment
+// x = x + 2 (Mesma coisa)
+x += 2
+ 
+// Subtraction assigment
+// x = x - 1 (Mesma coisa)
+x -= 1
+  
+// multiplication assigment
+// x = x * 2
+x *= 2
+   
+// division assigment
+// x = x / 2
+x /= 2
+
+// exponetiation
+x = x ** 2
+console.log(x)

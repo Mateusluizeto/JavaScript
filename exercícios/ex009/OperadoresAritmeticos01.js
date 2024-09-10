48f6560ad9fd0364dbd5a90e4a8d68507bd4467e
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-10 14:57:55 -0300

    Operadores aritmeticos

---
+++ exercícios/ex009/OperadoresAritmeticos01.js
@@ -0,0 +1,31 @@
+//Operadores Aritiméticos
+
+ // multiplicação ( * )
+ console.log(3.2 * 5.5)
+
+ // divisão ( / )
+ console.log(12 / 2)
+
+ // soma ( + )
+ console.log(34 + 67)
+
+ // subtração ( - )
+ console.log(34 - 23)
+  
+ // resto de divisão ( % )
+ console.log(11 % 3)
+
+ // incremento ( ++ )
+ let incremento = 0
+ incremento++ // Vai adcionar mais um
+ console.log(incremento) 
+
+ // decremento ( -- )
+ let decremento = 2
+ decremento-- // Vai tirar um
+ console.log(decremento)
+
+ // exponencial ( ** )
+ console.log(3 ** 2)
+ 
+

ea40526c156736322a6417ccaf0daa12fba6818c
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-10 14:56:42 -0300

    Manipulando arrays

---
+++ exercícios/ex007/prototype010.js
@@ -0,0 +1,9 @@
+// Manipulando Arrays 
+
+// Transformar uma cadeia de caracteres em um array
+
+let word = 'Manipulação'
+console.log(Array.from(word)) // Esse serve para tranformar cada caractere em um espaço separado no array
+
+
+// Resultado:  ['M', 'a', 'n', 'i', 'p', 'u', 'l', 'a', 'ç', 'ã', 'o']

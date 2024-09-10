57d8ddb3eb70b76b33ae8483d1aab5aaedf9ef39
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-10 14:54:55 -0300

    Operadores unário, binário e ternário

---
+++ exercícios/ex008/Expressions01.js
@@ -0,0 +1,14 @@
+/* Expressões e Operadores
+    - Expressions
+    - Operators
+        Binary
+        Unary
+        Ternary
+*/
+
+let number = 1
+
+console.log(++number) // Operador unários (tem 1 item)
+console.log(number + 1) // Operador binario (tem 2 itens)
+console.log(false ? 'oi' : 'nada') // Operador ternário (tem 3 itens) Esse é o if else simplificado
+

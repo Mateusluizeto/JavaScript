308f9dc40b6e2ff299340bfefb7c3eea4ef2095a
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-12 21:02:12 -0300

    Operador de string

---
+++ exercícios/ex009/OperadorString.js
@@ -0,0 +1,9 @@
+// Operador de string (String Operator)
+
+//comparison (comparação)
+    console.log('a' === 'a')
+
+// concatenation (concatenção)
+// Retorna a união de 2 strings
+let alpha = 'Alpha'
+console.log(alpha + 'bet' + 's')

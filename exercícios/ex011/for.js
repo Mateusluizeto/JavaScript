4e0366029445746732185220e6a0c665f5388e40
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-17 18:49:59 -0300

    Estrutura de repetição for

---
+++ exercícios/ex011/for.js
@@ -0,0 +1,6 @@
+// Estrutura de repetição 
+// for
+
+for(let i = 0; i <= 5; i++) {
+    console.log(i) // Resultado 0 1 2 3 4 5 
+}
---
+++ exercícios/ex011/for01.js
@@ -0,0 +1,12 @@
+// Estrutura de repetição 
+// for
+// break - Para a execução do loop
+// continue - Pula a execução do momento
+
+for(let i = 100; i >= 0; i--) {
+    if(i === 50 ){ // Isso faz com que o loop pare quando chegar no numero 50
+        break;
+    }
+
+    console.log(i)
+}

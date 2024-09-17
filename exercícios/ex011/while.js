8cf62b3208c9db346bdca09c480ff2f379b5ba07
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-17 19:30:58 -0300

    estrutura de repetição while

---
+++ exercícios/ex011/while.js
@@ -0,0 +1,9 @@
+// Estrutura de repetição
+// while
+
+let i = 0
+while(i < 10) { // enquanto i for menor que 10, ele vai executar o bloco de código abaixo
+    console.log(i) 
+    i+= 2
+    // Resultado: 0 2 4 6 8
+}

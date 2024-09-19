fdd2b006ca17415166f63bd490928e5fd78b3264
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-19 19:18:31 -0300

    forOf

---
+++ exercícios/ex011/forOf.js
@@ -0,0 +1,10 @@
+// for...of (CRIA UM LOOPS ATRAVÉS DE UMA VARIÁVEL)
+
+let nome = 'Mateus'
+let nomes = ['João', 'Paulo', 'Gabriel']
+
+for(let char of nomes[1]) { 
+    console.log(char) // Resultado: P a u l o
+}
+
+// Traduzindo o codigo dentro dos parênteses: Criar uma variável que vai conter um caractere da variavel nome

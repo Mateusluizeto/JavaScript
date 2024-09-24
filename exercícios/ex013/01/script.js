2a477387fd1ed16b6944fa4ed43baba191a66656
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-24 19:12:06 -0300

    Somando dois números

---
+++ exercícios/ex013/01/script.js
@@ -0,0 +1,9 @@
+/*
+    Solicite dois numeros para o ususario e faça a soma deles
+*/
+
+window.alert("Iremos somar dois numeros")
+let number1 = Number(window.prompt("Digite o primeiro número:"))
+let number2 = Number(window.prompt("Digite o segundo número:"))
+let result = number1 + number2
+window.alert(`Resultado final: ${result}`)

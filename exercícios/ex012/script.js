b4c1c78f3728d5dbda1f93e374d005921b66e62b
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-23 21:29:15 -0300

    JS no navegador

---
+++ exercícios/ex012/script.js
@@ -0,0 +1,8 @@
+/*
+    Encontre a solução ao problema:
+    Pergunte o nome de usuário e escreva a mensagem:
+    "Olá, [nome do usuário]"
+*/
+
+let nome = window.prompt("Qual é o seu nome?")
+window.alert(`Olá, ${nome}`)

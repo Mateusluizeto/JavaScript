5685654cf001eef6820166cc3aa6762bd75a3b98
Mateus Luizeto Reis <mluizetoreis@gmail.com>
2024-09-29 10:52:38 -0300

    Lista avançada

---
+++ exercícios/ex016/notes.md
@@ -0,0 +1,23 @@
+# Aperfeiçoando a resolução de problemas 
+
+Perguntas a serem feitas: 
+
+1. Quais os dados de entrada que terei? (inputs)
+    - Número desejado
+    - item da lista
+
+2. Quais as variáveis?
+    - Opção digitada
+    - lista de itens
+
+3. Qual o caminho ideal? (fluxo ideal)
+    - Os caminhos que não tem if else (Que é um caminho certo)
+
+4. Qual(is) o(s) caminho(s) alternativo(s)? (fluxo alternativo)
+    - Os caminhos que tem if else (Que tem mais de 1 opção de caminho)
+
+5. Quais tratamentos de dados preciso fazer? (estrutura de dados, funções)
+    - Transformar a string em um number
+
+6. Qual é a saída, ou quais os dados de saída, esperada? (outputs)
+    - Guardar os itens em uma lista
---
+++ exercícios/ex016/script.js
@@ -0,0 +1,52 @@
+/*
+    Faça um programam que tenha um menu e apresente a seguinte mensagem: 
+    
+    Olá usuário! Digite o número da opção desejada:
+
+       1. Cadastrar um item na lista
+       2. Mostrar itens cadastrados 
+       3. Sair do programa 
+
+    ---
+    O programa deverá capturar o número digitado pelo usuário e mostrar os seguintes
+    cenários: 
+
+    Caso o usuário digite 1, ele poderá cadastrar um item em uma lista.
+    Caso o usuário digite 2, ele poderá ver os itens cadastrados.
+      Se não houver nenhum item cadastrado, mostrar a mensagem:
+        "Não existem itens cadastrados"
+    Caso o usuário digite 3, a aplicação deverá ser encerrada.
+*/
+
+let option;
+let position = 0
+let items = []; // A lista de itens que a pessoa vai cadastrar
+
+while(option != 3){
+   option = Number(window.prompt(`Olá usuário! Digite o número da opção desejada:
+
+1. Cadastrar um item na lista
+2. Mostrar itens cadastrados 
+3. Sair do programa 
+`));
+
+    
+    if(option == 1){ // Cadastrar um item na lista
+        items[position] = window.prompt("Digite o nome do item:")
+    }
+     else if(option == 2){ // Ver os itens cadastrados
+    
+        if(items.length == 0){
+            window.alert("Não existem itens cadastrados")
+        } else{
+            window.alert(items) // Vai mostrar quais itens nós cadastramos
+        }
+    
+    } else {
+        window.alert("Tchau")
+    }
+    
+    console.log(items, option)
+    position++
+}
+
